6ce4da09047b2f1f61cd1ca573d5d8d514aae3d2
xiaohzangtongx <2446159171@qq.com>
2021-12-09 23:24:22 +0800

    项目初始化

---
+++ src/router/index.js
@@ -0,0 +1,76 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+/* Layout */
+import Layout from "@/layout";
+export const constantRoutes = [
+  {
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true,
+  },
+
+  {
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true,
+  },
+
+  {
+    path: "/",
+    component: Layout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: () => import("@/views/dashboard/index"),
+        meta: { title: "Dashboard", icon: "dashboard" },
+      },
+    ],
+  },
+
+  {
+    path: "/example",
+    component: Layout,
+    redirect: "/example/table",
+    name: "Example",
+    meta: { title: "Example", icon: "el-icon-s-help" },
+    children: [
+      {
+        path: "table",
+        name: "Table",
+        component: () => import("@/views/table/index"),
+        meta: { title: "Table", icon: "table" },
+      },
+    ],
+  },
+
+  {
+    path: "/form",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        name: "Form",
+        component: () => import("@/views/form/index"),
+        meta: { title: "Form", icon: "form" },
+      },
+    ],
+  },
+  // 404 page must be placed at the end !!!
+  { path: "*", redirect: "/404", hidden: true },
+];
+
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+  });
+
+const router = createRouter();
+
+export default router;
